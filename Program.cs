5e531bc14f2519ce31dee6144450db510618a421
AlexeyMischuk <gcbkjc@gmail.com>
2022-12-07 01:37:02 +0300

    first lines

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the Era name, like in the project view (e.g.: SteampunkVenice): ");
+string? eraName = Console.ReadLine();
+
+string villageDirectoryPath = $"{Path.DirectorySeparatorChar}cm-client{Path.DirectorySeparatorChar}CM-Unity{Path.DirectorySeparatorChar}Assets{Path.DirectorySeparatorChar}CoinMaster{Path.DirectorySeparatorChar}App{Path.DirectorySeparatorChar}Content{Path.DirectorySeparatorChar}Villages";
+string slotmachineDirectoryPath = $"{Path.DirectorySeparatorChar}cm-client{Path.DirectorySeparatorChar}CM-Unity{Path.DirectorySeparatorChar}Assets{Path.DirectorySeparatorChar}CoinMaster{Path.DirectorySeparatorChar}App{Path.DirectorySeparatorChar}Content{Path.DirectorySeparatorChar}SlotMachines";
+
+string villagePath = Path.Combine(Directory.GetCurrentDirectory(), villageDirectoryPath, eraName);
+string slotmachinePath = Path.Combine(Directory.GetCurrentDirectory(), slotmachineDirectoryPath, eraName);
